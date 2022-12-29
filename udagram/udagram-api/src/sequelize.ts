0dc41d932ee9fc4e1912f7e88f8b55c491793c88
AhmedMohammedKhalil <=>
2022-12-29 05:34:56 +0200

    init project

---
+++ udagram/udagram-api/src/sequelize.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize({
+	username: config.username,
+	password: config.password,
+	database: config.database,
+	port: config.db_port as unknown as number,
+	host: config.host,
+
+	dialect: "postgres",
+	storage: ":memory:",
+});
